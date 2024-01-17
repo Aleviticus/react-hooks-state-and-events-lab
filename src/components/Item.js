275fcb819f0ef1b9313691892fd08f3d08ff6347
Aaron Levy <aaronlevypc21@gmail.com>
2024-01-17 15:32:02 -0500

    completelab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { useState } from "react"; 
+
+
 
 function Item({ name, category }) {
+
+  const [isIncart, setIsInCart] = useState(true)
+  function handleClick(){
+    setIsInCart((isIncart) => !isIncart)
+    console.log('cart is clicked')
+  }
+
+  const buttonText = isIncart ? "Add to Cart" : "Remove From Cart "
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/* 
+      Implement ternary to change button's text based on
+      value of `isInCart`. 
+      */}
+      <button onClick={handleClick} className="add">{buttonText}</button>
     </li>
   );
 }
